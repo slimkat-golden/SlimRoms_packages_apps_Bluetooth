9787b0ad9ba494773c3b99290877021f5f5ea7e3
Cheney Ni <cheneyni@google.com>
2019-08-23 23:05:19 +0800

    AdapterService: Check the PIN code length before using
    The length is assigned by the framework. We should be better to check
    again before using, and dropped any unexcepted input.
    
    Bug: 139287605
    Test: PoC, atest -t BluetoothInstrumentationTests:com.android.bluetooth.btservice
    Change-Id: Ie2dd01e0b192e7ed1fe4b464618ddfa415dbf15c
    (cherry picked from commit d6c84aa34962333448e0ed8e4ddbc9de8b73c5ac)

--- src/com/android/bluetooth/btservice/AdapterService.java
+++ src/com/android/bluetooth/btservice/AdapterService.java
@@ -1279,6 +1279,12 @@ public class AdapterService extends Service {
             return false;
         }
 
+        if (pinCode.length != len) {
+            android.util.EventLog.writeEvent(0x534e4554, "139287605", -1,
+                    "PIN code length mismatch");
+            return false;
+        }
+
         byte[] addr = Utils.getBytesFromAddress(device.getAddress());
         return pinReplyNative(addr, accept, len, pinCode);
     }
@@ -1287,6 +1293,12 @@ public class AdapterService extends Service {
         enforceCallingOrSelfPermission(BLUETOOTH_PERM, "Need BLUETOOTH permission");
         DeviceProperties deviceProp = mRemoteDevices.getDeviceProperties(device);
         if (deviceProp == null || deviceProp.getBondState() != BluetoothDevice.BOND_BONDING) {
+            return false;
+        }
+
+        if (passkey.length != len) {
+            android.util.EventLog.writeEvent(0x534e4554, "139287605", -1,
+                    "Passkey length mismatch");
             return false;
         }
 
